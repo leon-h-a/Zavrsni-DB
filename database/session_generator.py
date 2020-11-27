61d9700b99264393b31dc2dbeb266770feb303cb
Naponoel <lekileo@gmail.com>
2020-11-27 09:30:57 +0100

    Basic working example with multiple subscribe/on_message topics and functions - modified for remote test

--- database/session_generator.py
+++ database/session_generator.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 # from webapp import config
 # todo: admin localhost i ostalo stavit u config.py
-DATABASE_URI = 'postgres+psycopg2://postgres:' + Config.psql_user + '@' + Config.psql_ip + ':' + Config.psql_port + '/' + Config.psql_db_name
+DATABASE_URI = 'postgres+psycopg2://' + Config.psql_user + ':' + Config.psql_pass + '@' + Config.psql_ip + ':' + Config.psql_port + '/' + Config.psql_db_name
 
 engine = create_engine(DATABASE_URI, echo=True)
 Session = sessionmaker(bind=engine)
