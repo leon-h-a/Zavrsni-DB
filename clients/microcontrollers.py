1428fec33eee72f76cf0952f4ddc21c7fba851a1
Naponoel <lekileo@gmail.com>
2020-11-29 19:57:38 +0100

    Ready

--- clients/microcontrollers.py
+++ clients/microcontrollers.py
@@ -1,36 +1,40 @@
 from database.session_generator import session_scope
 from models.microcontrollers import Microcontroller, Pin
+from sqlalchemy.orm import exc
 
+MQTT_ACTIVE_CLIENTS = [("init", 0), ("command_confirm", 0)]
 
-MQTT_ACTIVE_CLIENTS = [("init", 0)]
+default_pins = [
+    Pin(embeded_pin_name="PSWM1", io_type="INPUT", used_for=None, current_value=None, active=False),
+    Pin(embeded_pin_name="PSWM2", io_type="INPUT", used_for=None, current_value=None, active=False),
+    Pin(embeded_pin_name="PA1", io_type="INPUT", used_for=None, current_value=None, active=False),
+    Pin(embeded_pin_name="PA2", io_type="INPUT", used_for=None, current_value=None, active=False),
+    Pin(embeded_pin_name="PR1", io_type="OUTPUT", used_for=None, current_value=None, active=False),
+    Pin(embeded_pin_name="PR2", io_type="OUTPUT", used_for=None, current_value=None, active=False),
+    Pin(embeded_pin_name="PS", io_type="INPUT", used_for=None, current_value=None, active=False),
+    Pin(embeded_pin_name="PPMW1", io_type="OUTPUT", used_for=None, current_value=None, active=False),
+    Pin(embeded_pin_name="PPMW2", io_type="OUTPUT", used_for=None, current_value=None, active=False),
+    Pin(embeded_pin_name="PP1", io_type="INPUT", used_for=None, current_value=None, active=False),
+    Pin(embeded_pin_name="PP2", io_type="INPUT", used_for=None, current_value=None, active=False)
+]
 
 
 def initialisation(client, userdata, msg):
-    default_pins = [
-        Pin(embeded_pin_name="PSWM1", io_type="INPUT", used_for=None, current_value=None, active=False),
-        Pin(embeded_pin_name="PSWM2", io_type="INPUT", used_for=None, current_value=None, active=False),
-        Pin(embeded_pin_name="PA1", io_type="INPUT", used_for=None, current_value=None, active=False),
-        Pin(embeded_pin_name="PA2", io_type="INPUT", used_for=None, current_value=None, active=False),
-        Pin(embeded_pin_name="PR1", io_type="OUTPUT", used_for=None, current_value=None, active=False),
-        Pin(embeded_pin_name="PR2", io_type="OUTPUT", used_for=None, current_value=None, active=False),
-        Pin(embeded_pin_name="PS", io_type="INPUT", used_for=None, current_value=None, active=False),
-        Pin(embeded_pin_name="PPMW1", io_type="OUTPUT", used_for=None, current_value=None, active=False),
-        Pin(embeded_pin_name="PPMW2", io_type="OUTPUT", used_for=None, current_value=None, active=False),
-        Pin(embeded_pin_name="PP1", io_type="INPUT", used_for=None, current_value=None, active=False),
-        Pin(embeded_pin_name="PP2", io_type="INPUT", used_for=None, current_value=None, active=False)
-    ]
-    new_uC = Microcontroller(mac_address=msg.payload.decode('UTF-8'), controller_name=None, active=False)
-    new_uC.pins = default_pins
-    with session_scope() as sess:
-        sess.add(new_uC)
-        sess.commit()
-    print("Added new microcontroller: " + msg.payload.decode('UTF-8'))
+    with session_scope() as s:
+        try:
+            duplicate = s.query(Microcontroller).filter_by(mac_address=msg.payload.decode('UTF-8')).one()
+            if duplicate:
+                print("Microcontroller " + msg.payload.decode('UTF-8') + " already in database")
+        except exc.NoResultFound:
+            new_uC = Microcontroller(mac_address=msg.payload.decode('UTF-8'), controller_name=None, active=False)
+            new_uC.pins = default_pins
+            s.add(new_uC)
+            s.commit()
+            print("Added new microcontroller: " + msg.payload.decode('UTF-8'))
 
 
-def get_active_clients():
-    # todo: ovo se izvalci iz baze ovisno o tome jeli kontroler aktivan
-    raise NotImplemented
-
-
-def uc_state(client, userdata, message):
-    raise NotImplemented
+def command_confirm(client, userdata, msg):
+    msg.payload.decode('UTF-8')
+    print(msg.payload.decode('UTF-8'))
+    # with session_scope() as s:
+    #     terget_pin = s.query(Pin).filter_by(mac_address=msg.payload.decode('UTF-8')).one()
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from config import client
-from clients.microcontrollers import initialisation
-from clients.microcontrollers import MQTT_ACTIVE_CLIENTS
+from clients.microcontrollers import initialisation, command_confirm
 from models import Base
 from database.session_generator import engine
+from clients.microcontrollers import MQTT_ACTIVE_CLIENTS
 
 
 """ General functions """
-Base.metadata.drop_all(engine)
-Base.metadata.create_all(engine)
+# Base.metadata.drop_all(engine)
+# Base.metadata.create_all(engine)
 
 
 # todo: add timestamps in embedded
@@ -18,6 +18,7 @@ def broker_info(client, userdata, flags, rc):
 
 client.on_connect = broker_info
 client.message_callback_add("init", initialisation)
+client.message_callback_add("command_confirm", command_confirm)
 
 client.connect("172.105.76.166", 1883, 60)
 
